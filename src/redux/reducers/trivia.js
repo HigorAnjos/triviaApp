7ed896a45f92669fd946b4374d5f4db9154b1e14
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-03-10 15:16:30 -0300

    redux: finalizado

--- src/redux/reducers/trivia.js
+++ src/redux/reducers/trivia.js
@@ -1,7 +1,30 @@
-const INITIAL_STATE = '';
+import { TRIVIA_ERROR, TRIVIA_FETCHING, TRIVIA_SUCCESS } from '../actions';
+
+const INITIAL_STATE = {
+  isFetching: false,
+  error: undefined,
+  value: '',
+};
 
 const trivia = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+  case TRIVIA_FETCHING:
+    return {
+      ...state,
+      isFetching: true,
+    };
+  case TRIVIA_SUCCESS:
+    return {
+      ...state,
+      isFetching: false,
+      value: action.payload,
+    };
+  case TRIVIA_ERROR:
+    return {
+      ...state,
+      isFetching: false,
+      error: action.payload,
+    };
   default:
     return state;
   }
