de7209f94876857d9e6df3eff9a254dac3359c2b
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-03-10 15:38:08 -0300

    service: gravatar.js corrigido

--- src/services/gravatar.js
+++ src/services/gravatar.js
@@ -1,5 +1,7 @@
 import md5 from 'crypto-js/md5';
 
-const getGravatarUrl = (email) => md5(email).toString();
+const getGravatarUrl = (email) => (
+  `https://www.gravatar.com/avatar/${md5(email).toString()}`
+);
 
 export default getGravatarUrl;
