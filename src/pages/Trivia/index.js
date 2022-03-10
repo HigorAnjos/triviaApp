9d3b6805c5d4c404fae794dfd815cda97df487d0
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-03-10 20:54:36 -0300

    Logica de obtençao do token em todos os casos implementada

--- src/pages/Trivia/index.js
+++ src/pages/Trivia/index.js
@@ -1,15 +1,59 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getToken, setToken } from '../../services/localstorage';
+
 import Header from '../../components/Header';
+import { fetchToken, successFetchingToken } from '../../redux/actions';
 
 export class Trivia extends Component {
+  componentDidMount() {
+    const { dispatchSetToken, dispatchFetchToken } = this.props;
+    const token = getToken() || '';
+    if (!token) {
+      dispatchFetchToken();
+    } else {
+      dispatchSetToken(token);
+    }
+  }
+
+  componentDidUpdate() {
+    if (!getToken()) {
+      const { token } = this.props;
+      setToken(token);
+    }
+  }
+
   render() {
+    const { token } = this.props;
+
     return (
       <div>
         <Header />
         Trivia
+        <p>{ token }</p>
       </div>
     );
   }
 }
 
-export default Trivia;
+Trivia.propTypes = {
+  token: PropTypes.string,
+  dispatchFetchToken: PropTypes.func.isRequired,
+  dispatchSetToken: PropTypes.func.isRequired,
+};
+
+Trivia.defaultProps = {
+  token: '',
+};
+
+const mapStateToProps = (state) => ({
+  token: state.token.value,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  dispatchFetchToken: () => dispatch(fetchToken()),
+  dispatchSetToken: (token) => dispatch(successFetchingToken(token)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Trivia);
