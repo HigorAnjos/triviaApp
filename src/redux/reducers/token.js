c4e4c33dfabde3bb88c1fc8b0413a6eaf8a537e3
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-03-11 12:53:38 -0300

    Requisito 2 correçao do reducer token para adequaçao aos requisitos

--- src/redux/reducers/token.js
+++ src/redux/reducers/token.js
@@ -1,30 +1,11 @@
-import { TOKEN_ERROR, TOKEN_FETCHING, TOKEN_SUCCESS } from '../actions';
+import { TOKEN_SUCCESS } from '../actions';
 
-const INITIAL_STATE = {
-  isFetching: false,
-  error: undefined,
-  value: '',
-};
+const INITIAL_STATE = '';
 
 const token = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-  case TOKEN_FETCHING:
-    return {
-      ...state,
-      isFetching: true,
-    };
   case TOKEN_SUCCESS:
-    return {
-      ...state,
-      isFetching: false,
-      value: action.payload,
-    };
-  case TOKEN_ERROR:
-    return {
-      ...state,
-      isFetching: false,
-      error: action.payload,
-    };
+    return action.payload;
   default:
     return state;
   }
