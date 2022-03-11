e489a2bbcfb01c8257970bc069e5582e1c1b0780
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-03-10 22:54:24 -0300

    requisito 17e18 lint

--- src/pages/Ranking/index.js
+++ src/pages/Ranking/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { getPrevRanking } from '../../services/localstorage';
+
 export class Ranking extends Component {
   constructor() {
     super();
