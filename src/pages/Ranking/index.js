3513aefec812a3f3e5d38e1c3bc07444b279b0e8
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-03-10 22:51:47 -0300

    requisito 17e18 pagina de ranking

--- src/pages/Ranking/index.js
+++ src/pages/Ranking/index.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { getPrevRanking } from '../../services/localstorage';
 export class Ranking extends Component {
+  constructor() {
+    super();
+    this.state = {
+      storage: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      storage: getPrevRanking(),
+    });
+  }
+
   handleClick = () => {
     const { history } = this.props;
     history.push('/');
+  }
+
+  rankingList = () => {
+    const { storage } = this.state;
+    const storageSort = storage.sort((a, b) => {
+      if (a.score < b.score) {
+        const ONE = 1;
+        return ONE;
+      }
+      if (a.score > b.score) {
+        const ONELESS = -1;
+        return ONELESS;
+      }
+      return 0;
+    });
+    return storageSort.map(({ name, score, picture }, index) => (
+      <div key={ index }>
+        <img src={ picture } alt="imagem de perfil do jogador" />
+        <h1 data-testid={ `player-name-${index}` }>{ name }</h1>
+        <h2 data-testid={ `player-score-${index}` }>{ score }</h2>
+      </div>
+    ));
   }
 
   render() {
@@ -16,6 +52,9 @@ export class Ranking extends Component {
         >
           inicio
         </button>
+        {
+          this.rankingList()
+        }
       </main>
     );
   }
