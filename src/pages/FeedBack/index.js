6516844f2aa39bb80acde5788b469ad8ccfe03c9
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-03-10 21:16:00 -0300

    requisito 14

--- src/pages/FeedBack/index.js
+++ src/pages/FeedBack/index.js
@@ -3,25 +3,36 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class FeedBack extends React.Component {
-  render() {
+  feedback = () => {
     const { assertions } = this.props;
-    const TREEASSERTIONS = 3;
+    const THREEASSERTIONS = 3;
 
-    if (assertions < TREEASSERTIONS) {
+    if (assertions < THREEASSERTIONS) {
       return (<h1 data-testid="feedback-text">Could be better...</h1>);
     }
-    if (assertions > TREEASSERTIONS) {
-      return (<h1 data-testid="feedback-text">Well Done!</h1>);
-    }
+    return (<h1 data-testid="feedback-text">Well Done!</h1>);
+  }
+
+  render() {
+    const { assertions, score } = this.props;
+    return (
+      <main>
+        { this.feedback() }
+        <h2>{ `Voce acertou ${assertions} quetões!`}</h2>
+        <h2>{ `Um total de ${score} pontos` }</h2>
+      </main>
+    );
   }
 }
 
 FeedBack.propTypes = {
   assertions: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   assertions: state.player.assertions,
+  score: state.player.score,
 });
 
 export default connect(mapStateToProps, null)(FeedBack);
