d326b91f8d6a566c53b277d3cabf001596e9584d
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-03-10 22:07:23 -0300

    requisito 17

--- src/pages/Ranking/index.js
+++ src/pages/Ranking/index.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 export class Ranking extends Component {
+  handleClick = () => {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
-      <div>Ranking</div>
+      <main>
+        <button
+          data-testid="btn-go-home"
+          type="button"
+          onClick={ this.handleClick }
+        >
+          inicio
+        </button>
+      </main>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Ranking;
