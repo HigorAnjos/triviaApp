4fa0d197c0509f330b4a1f6d2ccb5fdf7a8407e6
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-03-13 11:20:26 -0300

    botao handleClickAnswer

--- src/components/Question/index.js
+++ src/components/Question/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes, { oneOfType } from 'prop-types';
+import { connect } from 'react-redux';
+import { setScore } from '../../redux/actions';
 
 const ANSWERS_ARRAY_SIZE = 4;
 const CORRECT_ANSWER = 'correct-answer';
 const ONE_SECOND = 1000;
+let interval;
 
 class Question extends React.Component {
   state = {
@@ -12,7 +15,7 @@ class Question extends React.Component {
   };
 
   componentDidMount() {
-    const interval = setInterval(() => {
+    interval = setInterval(() => {
       const { timer } = this.state;
       if (timer <= 0) {
         clearInterval(interval);
@@ -24,6 +27,19 @@ class Question extends React.Component {
     }, ONE_SECOND);
   }
 
+  handleClickAnswer = ({ target: { value } }) => {
+    const { timer } = this.state;
+    const { dispatchSetScore, question: { difficulty } } = this.props;
+    const difficultyPoints = { hard: 3, medium: 2, easy: 1 };
+
+    if (value === 'true') {
+      // para o timer
+      clearInterval(interval);
+      // set o score se acertou no redux
+      dispatchSetScore(timer, difficultyPoints[difficulty]);
+    }
+  }
+
   renderMultipleAnswers(correct, incorrectList) {
     const { randomCorrectIndex, timer } = this.state;
     const answersList = [...incorrectList];
@@ -32,9 +48,11 @@ class Question extends React.Component {
     return answersList.map((answer, index) => (
       <button
         type="button"
+        onClick={ this.handleClickAnswer }
         className="unselected-answer"
         disabled={ timer < 1 }
         key={ index }
+        value={ (index === randomCorrectIndex) }
         data-testid={ index === randomCorrectIndex
           ? CORRECT_ANSWER
           : `wrong-answer${incorrectList.indexOf(answer)}` }
@@ -55,8 +73,10 @@ class Question extends React.Component {
             <button
               type="button"
               key={ index }
+              onClick={ this.handleClickAnswer }
               className="unselected-answer"
               disabled={ timer < 1 }
+              value={ (answer === correct) }
               data-testid={ answer === correct
                 ? CORRECT_ANSWER
                 : 'wrong-answer-0' }
@@ -72,6 +92,7 @@ class Question extends React.Component {
   render() {
     const { question: { type, category, question: questionText,
       correct_answer: correctAnswer, incorrect_answers: incorrectAnswers } } = this.props;
+    const { timer } = this.state;
 
     return (
       <main>
@@ -84,6 +105,16 @@ class Question extends React.Component {
           {type === 'multiple'
             ? this.renderMultipleAnswers(correctAnswer, incorrectAnswers)
             : this.renderBoolAnswers(correctAnswer)}
+          <div>
+            { timer }
+          </div>
+          <div>
+            <button
+              type="button"
+            >
+              Próxima
+            </button>
+          </div>
         </section>
       </main>
     );
@@ -95,6 +126,11 @@ Question.propTypes = {
     PropTypes.string,
     PropTypes.array,
   ])).isRequired,
+  dispatchSetScore: PropTypes.func.isRequired,
 };
 
-export default Question;
+const mapDispatchToProps = (dispatch) => ({
+  dispatchSetScore: (timer, difficulty) => dispatch(setScore(timer, difficulty)),
+});
+
+export default connect(null, mapDispatchToProps)(Question);
