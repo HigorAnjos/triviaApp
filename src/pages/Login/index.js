52622e6b1049995b8045c137e09487035f8d6cfe
Gabriel Julio (GJ) <89492349+GJTrybe@users.noreply.github.com>
2022-03-17 05:47:08 -0300

    Login CSS

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,24 +2,28 @@ import propTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchToken, setUser } from '../../redux/actions';
+import avatar from './img_avatar2.png';
+import './style.css';
 
 class Login extends Component {
   state = {
     gravatarEmail: '',
     name: '',
-  }
+  };
 
   componentDidUpdate() {
     const { token, history } = this.props;
     const isFetchedToken = Boolean(token.length);
-    if (isFetchedToken) { history.push('/trivia'); }
+    if (isFetchedToken) {
+      history.push('/trivia');
+    }
   }
 
   handleChange = ({ target: { id, value } }) => {
     this.setState({
       [id]: value,
     });
-  }
+  };
 
   render() {
     const { gravatarEmail, name } = this.state;
@@ -27,50 +31,98 @@ class Login extends Component {
     const { dispatchSetUser, history, dispatchFetchToken } = this.props;
 
     return (
-      <main>
-        <form>
-          <label htmlFor="gravatarEmail">
-            Email do Gravatar:
-            <input
-              id="gravatarEmail"
-              value={ gravatarEmail }
-              onChange={ this.handleChange }
-              data-testid="input-gravatar-email"
-            />
-          </label>
-          <label htmlFor="name">
-            Nome do Jogador:
-            <input
-              id="name"
-              value={ name }
-              onChange={ this.handleChange }
-              data-testid="input-player-name"
-            />
-          </label>
+      <div className="container-form">
+        <form
+          action="/action_page.php"
+          method="post"
+          className="container-form-box"
+        >
+          <header className="container-form-header">
+            <img src={ avatar } alt="Avatar" className="img-avatar" />
+          </header>
 
-          <button
-            type="button"
-            disabled={ isDisabledButton }
-            onClick={ () => {
-              dispatchSetUser(this.state);
-              dispatchFetchToken();
-            } }
-            data-testid="btn-play"
-          >
-            Jogar
-          </button>
+          <main className="container-form-main">
+            <label
+              htmlFor="gravatarEmail"
+              className="container-form-main-label1"
+            >
+              <input
+                className="container-form-main-label1-input1"
+                id="gravatarEmail"
+                value={ gravatarEmail }
+                onChange={ this.handleChange }
+                data-testid="input-gravatar-email"
+                type="email"
+                placeholder="Email do Gravatar:"
+                name="gravatarEmail"
+                required
+              />
+            </label>
 
-          <button
-            type="button"
-            data-testid="btn-settings"
-            onClick={ () => {
-              history.push('/Configuration');
-            } }
-          >
-            Configurações
-          </button>
+            <label htmlFor="name" className="container-form-main-label2">
+              <input
+                className="container-form-main-label2-input1"
+                id="name"
+                value={ name }
+                onChange={ this.handleChange }
+                data-testid="input-player-name"
+                type="text"
+                placeholder="Nome do Jogador:"
+                name="name"
+                required
+              />
+            </label>
+
+            <button
+              className={
+                isDisabledButton
+                  ? 'container-form-main-button1" disabled'
+                  : 'container-form-main-button1'
+              }
+              id="play-Button"
+              type="button"
+              disabled={ isDisabledButton }
+              onClick={ () => {
+                dispatchSetUser(this.state);
+                dispatchFetchToken();
+              } }
+              data-testid="btn-play"
+            >
+              Jogar
+            </button>
+
+            <button
+              className="container-form-main-button2"
+              type="button"
+              data-testid="btn-settings"
+              onClick={ () => {
+                history.push('/Configuration');
+              } }
+            >
+              Configurações
+            </button>
+
+            <section className="container-form-section-remember">
+              <input
+                id="remember"
+                type="checkbox"
+                name="remember"
+                className="container-form-section-remember-input"
+              />
+              Lembrar-me
+            </section>
+          </main>
+
+          <footer className="container-form-footer">
+            <a
+              href="https://www.google.com.br/"
+              className="container-form-footer-a"
+            >
+              senha
+            </a>
+          </footer>
         </form>
-      </main>
+      </div>
     );
   }
 }
