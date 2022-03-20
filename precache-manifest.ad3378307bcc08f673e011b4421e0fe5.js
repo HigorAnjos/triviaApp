d29e6c7bc18dcc85996df16c4fc1a773c691efc0
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-03-19 21:14:45 -0300

    Updates

---
+++ precache-manifest.ad3378307bcc08f673e011b4421e0fe5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa67aead5b5392075c3fc6ed1309215",
+    "url": "/triviaApp/index.html"
+  },
+  {
+    "revision": "e10d999d4b11c5a5fb0b",
+    "url": "/triviaApp/static/css/main.3288d72d.chunk.css"
+  },
+  {
+    "revision": "971ed56523a38c2a07bd",
+    "url": "/triviaApp/static/js/2.5890e4e6.chunk.js"
+  },
+  {
+    "revision": "6b427be26341f18e5beeadca3932038e",
+    "url": "/triviaApp/static/js/2.5890e4e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e10d999d4b11c5a5fb0b",
+    "url": "/triviaApp/static/js/main.96ed9c70.chunk.js"
+  },
+  {
+    "revision": "27628e1c6d94e3e5e234",
+    "url": "/triviaApp/static/js/runtime-main.8f828b81.js"
+  },
+  {
+    "revision": "3973cd6b208c8e86365b47ddf09d85da",
+    "url": "/triviaApp/static/media/svg_avatar.3973cd6b.svg"
+  },
+  {
+    "revision": "20d560ac290993d4cc1e60ba7e95a0d5",
+    "url": "/triviaApp/static/media/tile_background.20d560ac.png"
+  }
+]);
