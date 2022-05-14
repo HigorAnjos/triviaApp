36cbff5c4cf84a9b782e249c9dace7b1105162c0
Higor Anjos <higorc.anjos@gmail.com>
2022-05-14 11:01:28 -0300

    branch protegida sera

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('VAI COMEÇAR', () => {
+test('VAI COMEÇAR hum', () => {
   // const { getByText } = render(<App />);
   // const linkElement = getByText(/SUA VEZ/i);
   // expect(linkElement).toBeInTheDocument();
