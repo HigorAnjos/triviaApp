bfe1e9ea84a435dc950736b76bb41e09880023cb
Higor Anjos <higorc.anjos@gmail.com>
2022-05-15 18:29:01 -0300

    feat(localstorage): getRanking test

--- src/tests/services/localstorage.test.js
+++ src/tests/services/localstorage.test.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 import { getRanking } from '../../services/localstorage';
 
-describe('Testing localStorage', () => {
+describe('Funcao getRanking', () => {
   // ref: https://gist.github.com/davidnguyen179/ee017fb1cf6659a920b40ec721498058
   let getFunc;
   let setFunc;
@@ -39,12 +39,12 @@ describe('Testing localStorage', () => {
     // Jest
   });
 
-  it('fn getRanking deve retornar um array vazio quando localstorage iniciado', () => {
+  it('fn deve retornar um array quando localstorage vazio', () => {
     getFunc.returns(null);
     expect(getRanking()).to.be.an('array');
   });
 
-  it('fn getRanking input json array retornar um array', () => {
+  it('fn input json array retornar um array', () => {
     const arrayJson = '[1, 2, 3]';
     // eslint-disable-next-line no-magic-numbers
     const expected = [1, 2, 3];
