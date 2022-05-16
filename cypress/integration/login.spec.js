33ed2da9a8e2409800a98e34c0ef433353a4d3a2
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-05-15 21:33:54 -0300

    feat(login): jogo trivia

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,22 +1,34 @@
 /// <reference types="Cypress" />
 
+// Variables
+
+const USER_EMAIL = 'higorc.anjos@gmail.com';
+const USER_NAME = 'Higor';
 describe('Login', () => {
   it('Conseguir efetuar login', () => {
-    const email = 'higorc.anjos@gmail.com';
-    const name = 'Higor';
 
     cy.visit('http://localhost:3000/triviaApp');
 
-    cy.get('[data-testid=input-gravatar-email]').type(email);
-    cy.get('[data-testid=input-gravatar-email]').should('have.value', email);
+    cy.get('[data-testid=input-gravatar-email]').type(USER_EMAIL);
+    cy.get('[data-testid=input-gravatar-email]').should('have.value', USER_EMAIL);
 
-    cy.get('#name').type(name);
-    cy.get('#name').should('have.value', name);
+    cy.get('#name').type(USER_NAME);
+    cy.get('#name').should('have.value', USER_NAME);
 
     cy.get('[data-testid=btn-play]').click();
 
     cy.url().should('eq', 'http://localhost:3000/trivia');
   })
+});
+
+describe('Jogo trivia', () => {
+  it('Nome digitado esteja visivel na pagina', () => {
+    cy.get('[data-testid=header-player-name]').should('have.text', USER_NAME);
+  });
+
+  it('Pontuação inicial de 0', () => {
+    cy.get('.player-score').should('have.text', 'Pontuação atual: 0');
+  });
 });
 
 // https://www.youtube.com/watch?v=gTRMuWCp8mE 23 minutos
