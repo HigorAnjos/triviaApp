38d8d73b1ef5b772d565039e1fd7ec0664d3c75f
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-05-15 21:19:43 -0300

    feat(login): test login

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,8 +1,21 @@
 /// <reference types="Cypress" />
 
 describe('Login', () => {
-  it('should login', () => {
-    cy.visit('http://localhost:3000/triviaApp')
+  it('Conseguir efetuar login', () => {
+    const email = 'higorc.anjos@gmail.com';
+    const name = 'Higor';
+
+    cy.visit('http://localhost:3000/triviaApp');
+
+    cy.get('[data-testid=input-gravatar-email]').type(email);
+    cy.get('[data-testid=input-gravatar-email]').should('have.value', email);
+
+    cy.get('#name').type(name);
+    cy.get('#name').should('have.value', name);
+
+    cy.get('[data-testid=btn-play]').click();
+
+    cy.url().should('eq', 'http://localhost:3000/trivia');
   })
 });
 
